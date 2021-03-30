86fa1fba07df20068e797357fbc7f906f14e6e4c
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-29 18:06:30 -0700

    aadded code to connect express server and mysql connection

---
+++ config/orm.js
@@ -0,0 +1,27 @@
+var connection = require("./connection.js")
+
+
+var orm ={
+    selectAll: function(tableName, cbModels){
+        connection.query("SELECT * from ??", tableName, function(err, burgerData){
+            cbModels(burgerData)
+        })
+    },
+    insertOne: function(tableName, body, cbModels){
+        connection.query("INSERT INTO ?? set ?", [tableName, body], function(err, burgerData){
+            cbModels(burgerData)
+        })
+    },
+    updateOne: function(tableName, updateObject, condition, cbModels){
+        connection.query("UPDATE ?? set ? WHERE ?", [tableName, updateObject, condition],function(err, burgerData){
+            cbModels(burgerData)
+        })
+    },
+    delete: function(tableName, condition, cbModels){
+        connection.query("DELETE FROM ?? WHERE ?", [tableName, condition],function(err, burgerData){
+            cbModels(burgerData)
+        })
+    }
+}
+
+module.exports= orm
---
+++ controllers/burgercontroller.js
@@ -0,0 +1,40 @@
+var burgerModel= require("../models/burger.js")
+var router = require("express").Router()
+
+router.get("/",function(req,res){
+    burgerModel.selectAll(function(burgerData){
+        res.render("index", {burgers: burgerData})
+    })
+})
+
+router.post("/api/burger", function(req,res){
+   req.body.devoured =false 
+    burgerModel.insertOne( req.body, function(burgerData){
+        res.redirect("/")
+    })
+}) 
+
+router.put("/api/burgers/:id", function(req,res){
+    var condition={
+        id:req.params.id
+    }
+    var updateObject={
+        devoured:true
+    }
+    burgerModel.updateOne(updateObject, condition, function(burgerData){
+        res.json(burgerData)
+    })
+})
+
+router.delete("/api/burgers/:id", function(req,res){
+    var deleteBurger={
+        id:req.params.id
+    }
+    burgerModel.delete( deleteBurger, function(burgerData){
+
+        res.json(burgerData)
+    })
+})
+
+
+module.exports= router
